ba185930cc3c54b8b7823ccca66ad5a1c26ad645
Michael Cowie <michael2437@hotmail.com>
2023-12-07 12:55:44 +1300

    Update single_dispatch.py
    Rewrite singledispatch

--- Python/Notes/single_dispatch.py
+++ Python/Notes/single_dispatch.py
@@ -1,90 +1,82 @@
 
-from functools import singledispatch
+ # singledispatch
  
- 
-@singledispatch
-def format_value(argument):
-    raise ValueError("Cannot write for {} type".format(type(argument)))
+`singledispatch` is a decorator aimed at functional programming. This decorator will transform a regular function into a single dispatch generic function. The `singledispatch` determines which registered function to call **based on the first argument type**.
 
-# REMEMBER: format_value = singledispatch(format_value)
+To utilize `singledispatch`, import it from `functools` and decorate a function with `@singledispatch`. From here, use the decorated function by registering the type in the form of `@decorated_function.register(type)`. Below is an example where I turn the function `add` into a `singledispatch` and have three registered types.
 
-#           calling format_value actually calls the "wrapper"
-#           method inside of single_dispatch,
-#
-#    def wrapper(*args, **kw):
-#        if not args:
-#            raise TypeError(f'{funcname} requires at least '
-#                            '1 positional argument')
-#
-#        return dispatch(args[0].__class__)(*args, **kw)
+```python
+from functools import singledispatch  
 
+@singledispatch  
+def add(a, b):  
+    raise NotImplementedError('Unsupported type')  
+  
+  
+@add.register(int)  
+def _(a, b):  
+    print("First argument is of type ", type(a))  
+    print(a + b)  
+  
+  
+@add.register(str)  
+def _(a, b):  
+    print("First argument is of type ", type(a))  
+    print(a + b)  
+  
+  
+@add.register(list)  
+def _(a, b):  
+    print("First argument is of type ", type(a))  
+    print(a + b)  
+  
+  
+if __name__ == '__main__':  
+    add(1, 2)                     # Calls @add.register(int) because of 1
+    add('Python', 'Programming')  # Calls @add.register(str) because of 'Python'
+    add([1, 2, 3], [5, 6, 7])     # Calls @add.register(list) because of [1, 2, 3]
+```
 
-"""
-Decorators MUST BE A CALLABLE FUNCTION, therefore this method works as so,
+From the examples above, remember that **the first parameter type determines which function is called**. If I were to change `add('Python', 'Programming')` to `add(1, 'Programming')`, then it would be calling `@add.register(int)` which would result in an error as we cannot concatenate a `int` and a `str`.
 
-def register(cls, func=None):
-    if func is None:         <------ func = None if using decorator
-        if isinstance(cls, type):
-            return lambda f: register(cls, f)
-                             int ------^   ^------ format_int
+When calling `add` with an unregistered type, then we will call the original decorated function, in this case it will raise an exception from `raise NotImplementedError('Unsupported type')`.
 
-            
-            @format_value.register(int)
-            @wrapper.register(int)
-            @lambda f: register(cls, f)
-                    ^--> Decorator callable, so f = format_int
-        =>  format_int = register(int, format_int)
-        
-        Now "[int] = format_int" is inside the registry
-        defined inside the singledispatch scope. From here,
-        you call format_value(5), NOT format_int. However
-        register(int, format_int) returns format_int, therefore
-        format_int(5) directly calls format_int
-"""
-@format_value.register(int)
-def format_int(value):
-    return f'Value = {value}: Type = int'
+# Different Ways to Register a Function
 
-"""
-OR we can call register directly as such to register
-the type -> method
-"""
-def format_str(value):
-    return f'Value = {value}: Type = str'
-format_value.register(str, format_str)
+In the examples above I use decorators to register all of my callbacks. This works by inspecting the source code and seeing why,
 
-"""
-Type annotation for the first argument is an alternative
-method to specifying the type for the method. Inside the register method
-it will access the annotations from "ann = getattr(cls, '__annotations__', {})"
-to grab "float" and access it to the function. Therefore, type annotations
-can be used by the Python itself and not only third party tools like IDE's.
-"""
-@format_value.register
-def format_float(value: float):
-    return f'Value = {value}: Type = float'
+```python
+def register(cls, func=None):  
+    """
+    generic_func.register(cls, func) -> func  
 
-print(format_value("123")) # Value = 123: Type = str
-print(format_value(456))   # Value = 456: Type = int
-print(format_value(5.5))   # Value = 5.5: Type = float
+	Registers a new implementation for the given *cls* on a *generic_func*.  
+	"""
+	nonlocal cache_token  
+    if _is_valid_dispatch_type(cls):  
+        if func is None:  
+            return lambda f: register(cls, f)  <--- When using a decorator, this will be returned
+        ...
+    ...
+```
 
-"""
-Single dispatch utilizes decorators and the fact that "everything" is
-an object in Python to add attributes to the function.
+Remember, when using decorators a function must be used. Therefore, `add.register(list)` must return a function. This is done from `return lambda f: register(cls, f)`. When we decorate our function as,
 
-format_value = singledispatch(format_value)
+```python
+@add.register(list)  
+def _(a, b):  
+    print("First argument is of type ", type(a))  
+    print(a + b)  
+```
 
-When we call format_value, we call 
+The `add.register(list)` will be changed to the lambda function where `f` is our function `_`. From here, it will implicitly call `_ = register(cls, _)`, where `cls` was the previously given `list`. However, this can also be done in a non-decorator approach by calling register directly, e.g.
 
-    def wrapper(*args, **kw):
-        if not args:
-            raise TypeError(f'{funcname} requires at least '
-                            '1 positional argument')
-        return dispatch(args[0].__class__)(*args, **kw)
-        
-where it will call the registered type to a method from dispatch.
+```python
+def add_list(a, b):  
+    print("First argument is of type ", type(a))  
+    print(a + b)  
+  
+add.register(list, func=add_list)
+```
 
-Calling register can be simplified as "registry[cls] = func"
-and calling format_value, which eventually calls dispatch
-will work as "return registry[cls]"
-"""
+Will also work. Hence, we have two different approaches to register the function, either a decorator or calling it directly.
