82a56911789a509797634730caa176b3533812e4
Michael Cowie <michael2437@hotmail.com>
2024-10-05 16:22:26 +1300

    Add installation using CMake

---
+++ Build/CMake/installation/example/src/main.cpp
@@ -0,0 +1,11 @@
+#include "my_header.h"
+#include <iostream>
+
+void print_message() {
+    std::cout << "Hello from MyProject!" << std::endl;
+}
+
+int main() {
+    print_message();
+    return 0;
+}
